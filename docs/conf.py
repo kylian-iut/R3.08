da7d273c2fbad21f826826835ce4f7e36bba9c8d
kylian-iut <kadam.iut@gmail.com>
2024-09-12 16:24:16 +0200

    sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0,os.path.abspath("../src"))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,7 +18,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc","sphinx.ext.viewcode"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +28,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
