748fd8e9af8339108c517692cda45fff706cc577
kylian-iut <kadam.iut@gmail.com>
2024-09-18 17:14:48 +0200

    17h00

---
+++ src/personnage.py
@@ -0,0 +1,9 @@
+class Personnage:
+    def __init__(self, psoeudo : str, niveau : int = 1, vie : int = 1, initiative : int = 1):
+        """
+            Méthode qui initialise le Personnage
+        :param psoeudo: Nom donné au joueur
+        :param niveau: Niveau d'avancement dans le jeu
+        :param vie: Quantité de vie avant l'élimination
+        :param initiative: jsp trop
+        """
