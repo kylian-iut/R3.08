6e461af8e39c94f8b7173699a2fe2f6bf2be27f6
kylian-iut <kadam.iut@gmail.com>
2024-09-19 09:25:44 +0200

    17h00

--- src/personnage.py
+++ src/personnage.py
@@ -1,9 +1,16 @@
 class Personnage:
-    def __init__(self, psoeudo : str, niveau : int = 1, vie : int = 1, initiative : int = 1):
+    def __init__(self, pseudo : str, niveau : int = 1):
         """
             Méthode qui initialise le Personnage
         :param psoeudo: Nom donné au joueur
         :param niveau: Niveau d'avancement dans le jeu
         :param vie: Quantité de vie avant l'élimination
-        :param initiative: jsp trop
+        :param initiative: C'est l'initiative
         """
+        self.__p = pseudo
+        self.__n = niveau
+        self.__v = niveau
+        self.__i = niveau
+
+
+
